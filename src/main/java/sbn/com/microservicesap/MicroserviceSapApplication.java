d31ebddb46ecc25c896772e8122363d769f889f6
Smirnov Boris <boris.smirnov@kriegerdigital.de>
2025-05-26 22:48:27 +0200

    Project_1 | Initial commit (to synchronize with a blank GitHub repository, that was created before).

---
+++ src/main/java/sbn/com/microservicesap/MicroserviceSapApplication.java
@@ -0,0 +1,13 @@
+package sbn.com.microservicesap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceSapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sbn/com/microservicesap/MicroserviceSapApplicationTests.java
@@ -0,0 +1,13 @@
+package sbn.com.microservicesap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
